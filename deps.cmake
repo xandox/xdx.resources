54184e0231aa9597c49798ca10d535fb1b193593
Eli Arzhannikov <iarzhannikov@gmail.com>
2018-04-27 15:55:53 +0300

    deps over https

--- deps.cmake
+++ deps.cmake
@@ -1,12 +1,12 @@
 
 xdx_source_dependency(
     xdx.cliopts
-    REPOSITORY "git@github.com:xandox/xdx.cliopts.git"
+    REPOSITORY "https://github.com/xandox/xdx.cliopts.git"
     BRANCH master
 )
 
 xdx_source_dependency(
     xdx.format
-    REPOSITORY "git@github.com:xandox/xdx.format.git"
+    REPOSITORY "https://github.com/xandox/xdx.format.git"
     BRANCH master
 )
